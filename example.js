ece07268aac8019d05a6bc2e401f411ac12afab5
Juliana Kavinya <kavinyajuliana990@gmail.com>
2024-07-08 17:07:52 +0300

    about  variables,datatypes,array and object in javascript

---
+++ example.js
@@ -0,0 +1,32 @@
+// statement;
+a = 10;
+
+// var variable name = value;
+var b = 20;
+
+// let variable name = value;
+c = 15;
+
+//const variable name = value;
+const pi =3.1452;
+
+// arthimetic operator
+// +, -, *, %, ++, --
+
+console.log(a+ b + c);
+console.log(a*b);
+console.log(++a);
+
+// Data types
+// numbers = integers,decimals
+// strings = characters
+// boolean = true/false
+// array = list of items (we use square brackets)
+var array = ["toyota","nissan","volvo"];
+console.log(array);
+// if you want to add index you do as shown below
+console.log(array[2]);
+
+// objects
+var person = {firstName:"Juliana", location: "Nairobi", course:"Computer Science"}
+
